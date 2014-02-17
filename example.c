31071c27a18ee1d88131c2d326d58cecaedc1646
pete911 <p.reisinger@gmail.com>
2014-02-17 17:14:12 +0000

    added generic code for keyboard table

--- example.c
+++ example.c
@@ -6,8 +6,10 @@
 
 int main(void)
 {
-    uint8_t d, mask, i;
-    uint8_t d_prev=0xFF;
+    uint8_t number_keys[10] =
+        {KEY_0, KEY_1, KEY_2, KEY_3, KEY_4, KEY_5, KEY_6, KEY_7, KEY_8, KEY_9};
+    uint8_t d, column, row;
+    uint8_t d_prev = 0xFF;
 
     CPU_PRESCALE(0);
 
@@ -17,29 +19,30 @@ int main(void)
     //  DDRx 1 - 0 low output 1 high output
     // PINx read the pin
 
-    //DDRD = 0x00;
-    //PORTD = 0xFF;
-    DDRD &= ~(1<<2);
-    PORTD |= (1<<2);
-
-    DDRB |= (1<<2);
-    // low output
-    //PORTB &= ~(1<<2);
     // high output
-    PORTB |= (1<<2);
+    DDRD = 0xFF;
+    PORTD = 0xFF;
+
+    // pullup resistor input
+    DDRB = 0x00;
+    PORTB = 0xFF;
 
     usb_init();
-    while (!usb_configured()) /* wait */ ;
+    while (!usb_configured());
     _delay_ms(1000);
 
     while (1) {
         d = PIND;
-        if (((d & (1<<2)) == 0) && (d_prev & (1<<2)) != 0) {
-        // this would work as well (without previous) but would
-        // type a lot of characters on single press. pullup resistor
-        // is 0 when connected to ground
-        //if ((d & (1<<2)) == 0) {
-            usb_keyboard_press(KEY_D, KEY_SHIFT);
+        for (column = 0; column < 8; column++) {
+            PORTD |= (1 << column);
+            for (row = 0; row < 8; row++) {
+                if (((d & (1 << row)) == 0) && (d_prev & (1 << row)) != 0) {
+                    usb_keyboard_press(number_keys[column], 0);
+                    usb_keyboard_press(number_keys[row], 0);
+                    usb_keyboard_press(KEY_SPACE, 0);
+                }
+            }
+            PORTD &= ~(1 << column);
         }
         d_prev = d;
         _delay_ms(2);
