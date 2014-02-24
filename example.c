74611ccffcbf9c3524e5ab7a666676e273d2a546
pete911 <p.reisinger@gmail.com>
2014-02-24 12:55:26 +0000

    split prev. rows into array

--- example.c
+++ example.c
@@ -9,7 +9,7 @@ int main(void)
     uint8_t number_keys[10] =
         {KEY_0, KEY_1, KEY_2, KEY_3, KEY_4, KEY_5, KEY_6, KEY_7, KEY_8, KEY_9};
     uint8_t column, row;
-    uint8_t pressed_columns, pressed_rows;
+    uint8_t pressed_rows[8];
 
     CPU_PRESCALE(0);
 
@@ -18,9 +18,6 @@ int main(void)
     //  DDRx 0 - 0 normal 1 pullup resistor
     //  DDRx 1 - 0 low output 1 high output
     // PINx read the pin
-
-    pressed_columns = 0x00;
-    pressed_rows = 0x00;
 
     // high output
     DDRD = 0xFF;
@@ -39,16 +36,14 @@ int main(void)
             PORTD &= ~(1 << column);
             for (row = 0; row < 8; row++) {
                 if ((PINB & (1 << row)) == 0) {
-                    if (((pressed_columns & (1 << column)) == 0) && ((pressed_rows & (1 << row)) == 0)) {
-                        pressed_columns |= (1 << column);
-                        pressed_rows |= (1 << row);
+                    if ((pressed_rows[row] & (1 << column)) == 0) {
+                        pressed_rows[row] |= (1 << column);
                         usb_keyboard_press(number_keys[column], 0);
                         usb_keyboard_press(number_keys[row], 0);
                         usb_keyboard_press(KEY_SPACE, 0);
                     }
-                } else if ((pressed_columns & (1 << column)) && (pressed_rows & (1 << row))) {
-                    pressed_columns &= ~(1 << column);
-                    pressed_rows &= ~(1 << row);
+                } else {
+                    pressed_rows[row] &= ~(1 << column);
                 }
             }
             PORTD |= (1 << column);
